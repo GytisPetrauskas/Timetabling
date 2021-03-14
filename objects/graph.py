2b430667d0ee0219d4087f94823a9399e7ae07bd
GytisPetrauskas <80126743+GytisPetrauskas@users.noreply.github.com>
2021-03-14 17:32:33 +0200

    Update graph.py

--- objects/graph.py
+++ objects/graph.py
@@ -1,13 +1,13 @@
 class Graph():
-	# Graph objects
+	# Graph object initialization
 	def __init__(self, vertex, neighbours, color, status):
 		self.vertex = vertex
 		self.neighbours = neighbours
 		self.color = color
 		self.status = status
-
+	# Creation of a whole graph;
 	def createGraph(activities, classrooms):
-		# Finding conflicts
+		# Finding conflicts - marking conflicting vertices as neighbours;
 		neighbours = []
 		for i in range(len(activities)):
 			neighbours.append([])
@@ -18,18 +18,17 @@ class Graph():
 					neighbours[i].append(j)
 				elif activities[i].subject != activities[j].subject and activities[i].lecturer == activities[j].lecturer and j not in neighbours[i]:
 					neighbours[i].append(j)	
-		# Adding available class list to every element of activity list
+		# Adding available class list to every element of activity list;
 		for i in activities:
 			available_classes = []
 			for j in classrooms:
 				if j.capacity >= i.group_size:
 					available_classes.append(j.classroom+':'+str(j.capacity)+':'+j.faculty)
 			i.classroom = available_classes
-		# Creating a list of graph objects
+		# Creating a list of Graph objects;
 		graph = []
 		for i in range(len(activities)):
 			graph.append(Graph(activities[i].subject, neighbours[i], None, 0))
-
 		return graph
 
 
